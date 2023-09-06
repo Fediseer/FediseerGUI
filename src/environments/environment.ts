1b2dc1481d82104dd255426eb776db5f347186bc
Dominik Chrastecky <dominik@chrastecky.cz>
2023-09-07 01:13:18 +0200

    Fix build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  apiUrl: process.env['FEDISEER_API_URL'] ?? 'https://fediseer.com/api',
-  apiVersion: process.env['FEDISEER_API_VERSION'] ?? 'v1',
-  appName: process.env['FEDISEER_GUI_APP_NAME'] ?? 'FediseerGUI',
-  appVersion: process.env['FEDISEER_GUI_APP_VERSION'],
-  maintainer: process.env['FEDISEER_GUI_APP_MAINTAINER'],
+  apiUrl: 'https://fediseer.com/api',
+  apiVersion: 'v1',
+  appName: 'FediseerGUI',
+  appVersion: '0.9.0',
+  maintainer: '@rikudou@lemmings.world',
 };
