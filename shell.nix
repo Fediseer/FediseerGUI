a4dee1385e67d0680f9ab114209f6d79ad2d1a63
Dominik Chrastecky <dominik@chrastecky.cz>
2023-09-07 11:06:45 +0200

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+    nativeBuildInputs = with pkgs.buildPackages; [
+      awscli
+      git
+      nodejs_18
+      nodePackages.serverless
+      nodePackages."@angular/cli"
+      yarn
+    ];
+}
