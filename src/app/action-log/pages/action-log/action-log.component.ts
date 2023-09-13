15a295124d91edfb51b322323ed26238a6e296ca
Dominik Chrastecky <dominik@chrastecky.cz>
2023-09-13 10:29:56 +0200

    Feat: Add pagination to action log

--- src/app/action-log/pages/action-log/action-log.component.ts
+++ src/app/action-log/pages/action-log/action-log.component.ts
@@ -5,6 +5,8 @@ import {ActionLogReportActivity, ActionLogReportType, ActionLogResponse} from ".
 import {toPromise} from "../../../types/resolvable";
 import {ApiResponseHelperService} from "../../../services/api-response-helper.service";
 import {MessageService} from "../../../services/message.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {int} from "../../../types/number";
 
 @Component({
   selector: 'app-action-log',
@@ -12,30 +14,65 @@ import {MessageService} from "../../../services/message.service";
   styleUrls: ['./action-log.component.scss']
 })
 export class ActionLogComponent implements OnInit {
+  private readonly apiPagesPerPage = 2;
+
   protected readonly ActionLogReportType = ActionLogReportType;
   protected readonly ActionLogReportActivity = ActionLogReportActivity;
 
   public loading: boolean = true;
   public actionLog: ActionLogResponse | null = null;
 
+  public pages: int[] = [1, 2, 3, 4, 5];
+  public currentPage: int = 1;
+  public lastPageReached: boolean = false;
+
   constructor(
     private readonly titleService: TitleService,
     private readonly api: FediseerApiService,
     private readonly messageService: MessageService,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly router: Router,
   ) {
   }
 
   public async ngOnInit(): Promise<void> {
     this.titleService.title = 'Action log';
 
-    const response = await toPromise(this.api.getActionLog(1, 5));
-    if (response === null) {
-      this.messageService.createError('Failed getting list of actions');
-      this.loading = false;
-      return;
-    }
+    this.activatedRoute.queryParams.subscribe(async params => {
+      this.loading = true;
 
-    this.actionLog = response;
-    this.loading = false;
+      this.currentPage = Number(params['page'] ?? 1);
+      const pageStart = ((this.currentPage - 1) * this.apiPagesPerPage) + 1;
+      const pageEnd = this.currentPage * this.apiPagesPerPage;
+
+      const response = await toPromise(this.api.getActionLog(pageStart, pageEnd));
+      if (response === null) {
+        this.messageService.createError('Failed getting list of actions');
+        this.loading = false;
+        return;
+      }
+      this.actionLog = response;
+      if (!this.actionLog.length) {
+        this.messageService.createWarning('No more pages available');
+        this.lastPageReached = true;
+        this.goToPage(this.currentPage - 1);
+        this.pages.pop();
+        return;
+      }
+
+      if (!this.lastPageReached && !this.pages.includes(this.currentPage + 1)) {
+        this.pages.push(this.currentPage + 1);
+      }
+
+      this.loading = false;
+    });
+  }
+
+  public goToPage(page: int): void {
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: {page: page},
+      queryParamsHandling: 'merge',
+    });
   }
 }
