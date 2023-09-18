0a7c4191807813207e3a6c2353f59faa88f79813
Dominik Chrástecký <dominik@chrastecky.cz>
2023-09-18 17:30:04 +0200

    Feat: Add filters to action log (#65)

--- src/app/action-log/pages/action-log/action-log.component.ts
+++ src/app/action-log/pages/action-log/action-log.component.ts
@@ -8,6 +8,11 @@ import {MessageService} from "../../../services/message.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {int} from "../../../types/number";
 import {range} from "../../../shared/helper/range";
+import {FormControl, FormGroup} from "@angular/forms";
+import {DatabaseService} from "../../../services/database.service";
+import {FilterSpecialValueAllInstances} from "../../../shared/constants";
+import {map} from "rxjs";
+import {response} from "express";
 
 @Component({
   selector: 'app-action-log',
@@ -23,9 +28,19 @@ export class ActionLogComponent implements OnInit {
   public loading: boolean = true;
   public actionLog: ActionLogResponse | null = null;
 
-  public pages: int[] = range(15);
+  public pages: int[] = range(5);
   public currentPage: int = 1;
   public lastPageReached: boolean = false;
+
+  public whitelistedDomains: string[] | null = null;
+  public blacklistedDomains: string[] | null = null;
+
+  public form = new FormGroup({
+    type: new FormControl<ActionLogReportType | null>(null),
+    activity: new FormControl<ActionLogReportActivity | null>(null),
+    sourceDomains: new FormControl<string[]>([]),
+    targetDomains: new FormControl<string[]>([]),
+  });
 
   constructor(
     private readonly titleService: TitleService,
@@ -33,20 +48,57 @@ export class ActionLogComponent implements OnInit {
     private readonly messageService: MessageService,
     private readonly activatedRoute: ActivatedRoute,
     private readonly router: Router,
+    private readonly apiResponseHelper: ApiResponseHelperService,
   ) {
   }
 
   public async ngOnInit(): Promise<void> {
     this.titleService.title = 'Action log';
 
+    const responses = await Promise.all([
+      toPromise(this.api.getWhitelistedInstances().pipe(
+        map (response => {
+          if (this.apiResponseHelper.handleErrors([response])) {
+            return [];
+          }
+
+          return response.successResponse!.instances.map(instance => instance.domain);
+        }),
+      )),
+      toPromise(this.api.getCensuredInstances().pipe(
+        map (response => {
+          if (this.apiResponseHelper.handleErrors([response])) {
+            return [];
+          }
+
+          return response.successResponse!.instances.map(instance => instance.domain);
+        }),
+      )),
+    ]);
+
+    this.whitelistedDomains = responses[0];
+    this.blacklistedDomains = responses[1];
+
     this.activatedRoute.queryParams.subscribe(async params => {
       this.loading = true;
-
       this.currentPage = Number(params['page'] ?? 1);
+
+      this.form.patchValue({
+        sourceDomains: params['sourceDomains'] ? params['sourceDomains'].split(',') : [],
+        targetDomains: params['targetDomains'] ? params['targetDomains'].split(',') : [],
+        activity: params['activity'] ?? null,
+        type: params['type'] ?? null,
+      });
+
       const pageStart = ((this.currentPage - 1) * this.apiPagesPerPage) + 1;
       const pageEnd = this.currentPage * this.apiPagesPerPage;
 
-      const response = await toPromise(this.api.getActionLog(pageStart, pageEnd));
+      const response = await toPromise(this.api.getActionLog(pageStart, pageEnd, {
+        targetDomains: this.form.controls.targetDomains.value ?? undefined,
+        sourceDomains: this.form.controls.sourceDomains.value ?? undefined,
+        activity: this.form.controls.activity.value ?? undefined,
+        type: this.form.controls.type.value ?? undefined,
+      }));
       if (response === null) {
         this.messageService.createError('Failed getting list of actions');
         this.loading = false;
@@ -56,8 +108,10 @@ export class ActionLogComponent implements OnInit {
       if (!this.actionLog.length) {
         this.messageService.createWarning('No more pages available');
         this.lastPageReached = true;
-        this.goToPage(this.currentPage - 1);
-        this.pages.pop();
+        if (this.currentPage !== 1) {
+          this.goToPage(this.currentPage - 1);
+          this.pages.pop();
+        }
         return;
       }
 
@@ -75,5 +129,30 @@ export class ActionLogComponent implements OnInit {
       queryParams: {page: page},
       queryParamsHandling: 'merge',
     });
+  }
+
+  public async filterLog(): Promise<void> {
+    let sourceDomains: string[] | undefined = this.form.controls.sourceDomains.value ?? [];
+    if (!sourceDomains.length) {
+      sourceDomains = undefined;
+    }
+    let targetDomains: string[] | undefined = this.form.controls.targetDomains.value ?? [];
+    if (!targetDomains.length) {
+      targetDomains = undefined;
+    }
+
+    const query = {
+      page: this.currentPage,
+      type: this.form.controls.type.value ?? undefined,
+      activity: this.form.controls.activity.value ?? undefined,
+      sourceDomains: sourceDomains?.join(','),
+      targetDomains: targetDomains?.join(','),
+    }
+
+    await this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: query,
+    });
+    this.lastPageReached = false;
   }
 }
