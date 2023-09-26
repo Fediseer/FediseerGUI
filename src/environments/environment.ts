f2dbdd1c8bd408aadf6218d5e4f24805344727be
Dominik Chrastecky <dominik@chrastecky.cz>
2023-09-26 13:46:43 +0200

    Increase version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   apiUrl: 'https://fediseer.com/api',
   apiVersion: 'v1',
   appName: 'FediseerGUI',
-  appVersion: '0.13.7',
+  appVersion: '0.13.8',
   maintainer: '@rikudou@lemmings.world',
   defaultCensuresListInstanceFilter: [FilterSpecialValueAllInstances],
   sourceCodeLink: 'https://github.com/Fediseer/FediseerGUI',
